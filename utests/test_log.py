c5f4bfe35189793165464155e9a3d6f52b843a55
Luca Giovenzana <luca@giovenzana.org>
2020-04-17 22:00:41 +0200

    Fixed first unit tests

--- utests/test_log.py
+++ utests/test_log.py
@@ -1,22 +1,23 @@
-from RequestsLibrary.log import format_data_to_log_string_according_to_headers
+from requests import Session
 from requests.utils import default_headers
+from RequestsLibrary.log import format_data_to_log_string_according_to_headers
 
-# FIXME those tests are broken because I started a refactor without
-# all tests and left the refactor uncompleted so I didn't trust the
-# refactor and reverted the code but not the tests :)
 
 def test_format_data_with_data_and_headers_none():
-    data = format_data_to_log_string_according_to_headers(None, None)
+    session = Session()
+    data = format_data_to_log_string_according_to_headers(session, None, None)
     assert data is None
 
 
 def test_format_data_with_headers_none():
-    data = format_data_to_log_string_according_to_headers('data', None)
+    session = Session()
+    data = format_data_to_log_string_according_to_headers(session, 'data', None)
     assert data is None
 
 
 def test_format_data_with_data_none():
-    data = format_data_to_log_string_according_to_headers(None, default_headers())
+    session = Session()
+    data = format_data_to_log_string_according_to_headers(session, None, default_headers())
     assert data is None
 
 
